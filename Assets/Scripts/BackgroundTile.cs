8700ea0e1c689d50682fd707b14bbcfed3e1aa3f
berke-ozcan <142122706+berke-ozcan@users.noreply.github.com>
2024-07-01 18:52:33 +0300

    yeni "oyun tahtası(?)"
    gelişme yok sadece aynı şeyi baştaan yaptım

--- Assets/Scripts/BackgroundTile.cs
+++ Assets/Scripts/BackgroundTile.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class BackgroundTile : MonoBehaviour
 {
+    public GameObject[] tiles;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Initialize()
+    {
+        int tileToUse = Random.Range(0,tiles.Length);
+        GameObject tile = Instantiate(tiles[tileToUse], transform.position, Quaternion.identity);
+
+        tile.transform.parent = this.transform;
+        tile.name = this.gameObject.name;
     }
 }
--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,11 +6,32 @@ public class Board : MonoBehaviour
 {
     public int height;
     public int width;
+    public GameObject tilePrefab;
+
+    private BackgroundTile[,] allTiles;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        allTiles = new BackgroundTile[height, width];
+        SetUp();
+    }
+
+    private void SetUp()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 tempPos = new Vector2(i, j);
+                GameObject bgTile = Instantiate(tilePrefab, tempPos, Quaternion.identity) as GameObject;
+
+                bgTile.transform.parent = this.transform;
+                bgTile.name = "(" + i + "," + j + ")";
+
+
+            }
+        }
     }
 
     // Update is called once per frame
