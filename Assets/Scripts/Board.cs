3a90df15d34759aadfe3f0303ffca30722eb8f52
berke-ozcan <142122706+berke-ozcan@users.noreply.github.com>
2024-07-01 18:11:20 +0300

    :(

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int height;
+    public int width;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
