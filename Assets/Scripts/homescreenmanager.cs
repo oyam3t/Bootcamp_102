bc6d03bae36c6a923e70d38fe1a0d01599de121d
berke-ozcan <142122706+berke-ozcan@users.noreply.github.com>
2024-08-01 16:59:52 +0300

    açılış sahnesi

---
+++ Assets/Scripts/homescreenmanager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homescreenmanager : MonoBehaviour
+{
+    public GameObject loadingscreen;
+    public GameObject mainscreen;
+    public GameObject playButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(wait());
+    }
+
+    private IEnumerator wait()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(loadingscreen);
+        mainscreen.SetActive(true);
+        playButton.SetActive(true);
+    }
+
+    public void playButtonOnClick()
+    {
+        Debug.Log("play button");
+        //DİĞER SAHNEYE GEÇ
+    }
+}
